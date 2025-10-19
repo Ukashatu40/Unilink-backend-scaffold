e8b4ebe2a396fa8c38fd8afc6cd9860d9342aea8
Ukashatu40 <ukashatuabdullahi40@gmail.com>
2025-10-19 15:32:01 +0100

    feat: Included Resigter and Login Buttons

--- smart-travel-frontend/src/components/Navbar.jsx
+++ smart-travel-frontend/src/components/Navbar.jsx
@@ -1,6 +1,16 @@
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 
 export default function Navbar() {
+
+  const navigate = useNavigate();
+  const token = localStorage.getItem("token");
+
+  const handleLogout = () => {
+    localStorage.removeItem("token");
+    localStorage.removeItem("user");
+    navigate("/login");
+  };
+
   return (
     <nav className="bg-blue-600 text-white p-4 shadow">
       <div className="container mx-auto flex justify-between items-center">
@@ -8,6 +18,16 @@ export default function Navbar() {
         <div className="space-x-4">
           <Link to="/" className="hover:text-gray-200">Dashboard</Link>
           <Link to="/reviews" className="hover:text-gray-200">Reviews</Link>
+        {!token ? (
+          <>
+            <Link to="/login">Login</Link>
+            <Link to="/register">Register</Link>
+          </>
+        ) : (
+          <button onClick={handleLogout} className="bg-red-500 px-3 py-1 rounded">
+            Logout
+          </button>
+        )}
         </div>
       </div>
     </nav>
