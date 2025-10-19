8ae899814fa1a095c7a00f7605bda1774fccaefc
Ukashatu40 <ukashatuabdullahi40@gmail.com>
2025-10-19 15:30:22 +0100

    feat: Included Register, Login routes while protecting the /reviews route

--- smart-travel-frontend/src/App.jsx
+++ smart-travel-frontend/src/App.jsx
@@ -2,6 +2,9 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import Dashboard from "./pages/Dashboard";
 import Reviews from "./pages/Reviews";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+import ProtectedRoute from "./components/ProtectedRoute";
 
 export default function App() {
   return (
@@ -9,7 +12,16 @@ export default function App() {
       <Navbar />
       <Routes>
         <Route path="/" element={<Dashboard />} />
-        <Route path="/reviews" element={<Reviews />} />
+        <Route
+          path="/reviews"
+          element={
+            <ProtectedRoute>
+              <Reviews />
+            </ProtectedRoute>
+          }
+        />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
       </Routes>
     </Router>
   );
